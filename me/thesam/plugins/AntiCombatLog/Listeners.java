bd4c0670fc0a203f5bf25bec36ca4c0a3db78693
SamOutabrae <Samuelross06@outlook.com>
2021-03-27 22:31:26 -0400

    Init commit.

---
+++ me/thesam/plugins/AntiCombatLog/Listeners.java
@@ -0,0 +1,13 @@
+package me.thesam.plugins.AntiCombatLog;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class Listeners implements Listener {
+	
+	@EventHandler
+	public void onDisconnect(PlayerQuitEvent e) {
+		
+	}
+}
