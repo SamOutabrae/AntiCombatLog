2fbaef0c5df3f35aa7fcdbede4bc8f78654d0093
SamOutabrae <Samuelross06@outlook.com>
2021-03-27 22:00:43 -0400

    Added getTimeout() method to DataLoader.

--- me/thesam/plugins/AntiCombatLog/DataLoader.java
+++ me/thesam/plugins/AntiCombatLog/DataLoader.java
@@ -1,6 +1,9 @@
 package me.thesam.plugins.AntiCombatLog;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class DataLoader {
 	
@@ -23,6 +26,26 @@ public class DataLoader {
 		return;
 	}
 	
-	
+	public int getTimeout() {
+		int timeout;
+		
+		try {
+		
+			File f = new File(path + "timeout.txt");
+			FileInputStream fIN = new FileInputStream(f);
+		
+			byte[] byteArray = fIN.readAllBytes();
+			
+			String fileData = new String(byteArray, StandardCharsets.UTF_8);
+			
+			timeout = Integer.parseInt(fileData);
+		
+		} catch(IOException e) {
+		
+			timeout = 20;
+		}
+		
+		return timeout;
+	}
 
 }
