3f551ee936ce4aed2b0b4ae5b2857bf8a0b927dd
SamOutabrae <Samuelross06@outlook.com>
2021-03-27 21:47:46 -0400

    Init commit.

---
+++ me/thesam/plugins/AntiCombatLog/Main.java
@@ -0,0 +1,20 @@
+package me.thesam.plugins.AntiCombatLog;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	
+	public static int combatTimeout;
+	
+	public static DataLoader dataLoader = new DataLoader();
+	
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+}
