ffefb3da4a702202f0d0d7ef771b4a2f183ffd96
SamOutabrae <Samuelross06@outlook.com>
2021-03-27 22:58:31 -0400

    Made Main register Listeners.

--- me/thesam/plugins/AntiCombatLog/Main.java
+++ me/thesam/plugins/AntiCombatLog/Main.java
@@ -1,5 +1,8 @@
 package me.thesam.plugins.AntiCombatLog;
 
+import java.util.ArrayList;
+
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
@@ -8,9 +11,11 @@ public class Main extends JavaPlugin {
 	
 	public static DataLoader dataLoader = new DataLoader();
 	
+	public ArrayList<Player> playersToKill = new ArrayList<Player>();
+	
 	@Override
 	public void onEnable() {
-		
+		getServer().getPluginManager().registerEvents(new Listeners(), this);
 	}
 	
 	@Override
