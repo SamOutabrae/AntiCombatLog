265e517272f47ce62bde40f094eca757e780065c
SamOutabrae <Samuelross06@outlook.com>
2021-03-27 22:21:22 -0400

    Changed DataLoader to use a config file insteadof a directory full of files.

--- me/thesam/plugins/AntiCombatLog/DataLoader.java
+++ me/thesam/plugins/AntiCombatLog/DataLoader.java
@@ -2,50 +2,69 @@ package me.thesam.plugins.AntiCombatLog;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+
+import javax.xml.stream.events.Characters;
 
 public class DataLoader {
 	
 	String path = new File(".").getAbsolutePath();
 	
-	public DataLoader() {
-		File f = new File(path + "AntiCombatLog");
+	HashMap<String, Object> configData = new HashMap<String, Object>();
+	
+	public DataLoader() throws FileNotFoundException {
 		
-		if(f.exists() && f.isDirectory()) {
-			return;
-		}
+		configData.put("timeout", null);
+		configData.put("playersOnly", null);
+		
+		File f = new File(path + "AntiCombatLog.conf");
 		
 		if(f.exists()) {
+		
 			f.delete();
 			f.mkdir();
-			return;
-		}
 		
-		f.mkdir();
+		} else {
+		
+			f.mkdir();
+		
+		}
+			
+		
+		try {
+			
+			FileInputStream fIN = new FileInputStream(f);
+			
+			byte[] byteArray = fIN.readAllBytes();
+			String fileData = new String(byteArray, StandardCharsets.UTF_8);
+			
+			String[] fileDataArray;
+			
+			fileDataArray = fileData.split(";");
+			
+			for(String line : fileDataArray) {
+				String[] config = line.split("=");
+				
+				configData.put(config[0], config[1]);
+				
+				continue;
+			}
+			
+			fIN.close();
+			
+		} catch(IOException e) {
+		
+			e.printStackTrace();
+			
+		}
+	
+		
 		return;
 	}
 	
-	public int getTimeout() {
-		int timeout;
 		
-		try {
-		
-			File f = new File(path + "timeout.txt");
-			FileInputStream fIN = new FileInputStream(f);
-		
-			byte[] byteArray = fIN.readAllBytes();
-			
-			String fileData = new String(byteArray, StandardCharsets.UTF_8);
-			
-			timeout = Integer.parseInt(fileData);
-		
-		} catch(IOException e) {
-		
-			timeout = 20;
-		}
-		
-		return timeout;
-	}
-
 }
